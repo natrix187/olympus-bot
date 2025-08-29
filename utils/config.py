d25d67907db7266e680dc219dbb0eca7abf18546
natrix187 <natrix.871@gmail.com>
2025-08-29 16:11:26 +1100

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -4,6 +4,6 @@ TOKEN = os.environ.get("TOKEN")
 NAME = "Olympus"
 server = "https://discord.com/invite/odx"
 ch = "https://discord.com/channels/699587669059174461/1271825678710476911"
-OWNER_IDS = [213347081799073793, 1070619070468214824]
+OWNER_IDS = [21334708179907379, 1070619070468214824]
 BotName = "Olympus"
 serverLink = "https://discord.com/invite/odx"
