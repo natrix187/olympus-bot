b0f39f1158fe1ba7ddad2af997bf319b268b7cf9
natrix187 <natrix.871@gmail.com>
2025-08-31 20:05:25 +1100

    Update Olympus.py

--- core/Olympus.py
+++ core/Olympus.py
@@ -59,7 +59,7 @@ class Olympus(commands.AutoShardedBot):
         await self.change_presence(status=discord.Status.do_not_disturb,
                                    activity=discord.Activity(
                                        type=discord.ActivityType.playing,
-                                       name='Bot cree par Natrix'))
+                                       name='Created by Natrix'))
 
     async def send_raw(self, channel_id: int, content: str,
                        **kwargs) -> typing.Optional[discord.Message]:
